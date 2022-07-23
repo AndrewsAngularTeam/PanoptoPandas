206918b2455d19490c4cd80f058c5a460d933e28
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-24 00:44:14 +1200

    finish leaderboard page

--- src/pages/Leaderboard/Leaderboard.js
+++ src/pages/Leaderboard/Leaderboard.js
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import LeaderboardPerson from "../../components/LeaderboardPerson/LeaderboardPerson";
 import classes from "./Leaderboard.module.scss";
+import Crown from "../../assets/crown.svg";
 
 const Leaderboard = () => {
 
@@ -101,15 +102,21 @@ const Leaderboard = () => {
                 <div className={classNames(classes.TopPlacing, classes.Second)}>
                     <img src={data[1].profileImage} />
                     <div className={classes.Badge}>2</div>
+                    <h3>{data[1].name}</h3>
+                    <p>{data[1].totalWatchTimeMin} <span>min</span></p>
                 </div>
                 <div className={classNames(classes.TopPlacing, classes.First)}>
+                    <img src={Crown} className={classes.Crown} />
                     <img src={data[0].profileImage} />
                     <div className={classes.Badge}>1</div>
-
+                    <h3>{data[0].name}</h3>
+                    <p>{data[0].totalWatchTimeMin}  <span>min</span></p>
                 </div>
                 <div className={classNames(classes.TopPlacing, classes.Third)}>
                     <img src={data[2].profileImage} />
                     <div className={classes.Badge}>3</div>
+                    <h3>{data[2].name}</h3>
+                    <p>{data[2].totalWatchTimeMin} <span>min</span></p>
 
 
                 </div>
