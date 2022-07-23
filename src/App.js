2ae97ae61803370d7dc66ae84d4850f8e756444d
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-23 14:58:35 +1200

    re-remove logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import { TimingProvider } from "./context/TimingContext";
 import TimerButtons from "./components/TimerButtons";
 import "./App.css";
@@ -22,7 +21,6 @@ function App() {
     <TimingProvider>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
