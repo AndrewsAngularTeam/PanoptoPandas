745a5cd9b44af232aca15216d67de9e8aa4e08f1
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-24 02:38:21 +1200

    add selected and owned states for products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,17 +4,37 @@ import Gem from "../../assets/gem.svg"
 import PlaceholderProduct from "../../assets/placeholderProduct.png"
 import Lock from "../../assets/lock.svg"
 import Unlock from "../../assets/unlock.svg"
+import classNames from 'classnames';
+import Check from "../../assets/check.svg"
 
 const Product = (props) => {
+
+    const isUnlocked = props.owned || props.isSelected
+    console.log("props", props)
     return (
-        <div className={classes.Product}>
+        <div className={
+            isUnlocked ?
+                classNames(classes.Product, classes.Active) :
+                classNames(classes.Product)
+        }>
             <img src={PlaceholderProduct} />
             <div className={classes.ProductInfo}>
-                <div className={classes.PriceContainer}>
-                    <img src={Gem} />
-                    <p>600</p>
-                </div>
-                <img src={Lock} />
+                {
+                    isUnlocked ?
+                        <div className={classes.CheckboxContainer}>
+                            <button
+                                className={
+                                    props.isSelected ? classes.Selected : null}>
+                                <img src={Check} />
+                            </button>
+                            {props.isSelected ? <p>Applied</p> : <p>Use</p>}
+                        </div> :
+                        <div className={classes.PriceContainer}>
+                            <img src={Gem} />
+                            <p>600</p>
+                        </div>
+                }
+                <img src={isUnlocked ? Unlock : Lock} />
             </div>
         </div>
     )
