8451a1208420c7fcde16bf84a211b391bfdc34f4
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-24 01:29:07 +1200

    add profile container for settings page

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,9 +1,16 @@
 import classes from "./Settings.module.scss";
+import PlaceholderAvatar from "../../assets/placeholderAvatar.png"
 
 const Settings = (props) => {
     return (
         <div className={classes.Settings}>
-            <div className={classes.ProfileContainer}>prof</div>
+            <div className={classes.ProfileContainer}>
+                <img src={PlaceholderAvatar} />
+                <div className={classes.ProfileInfo}>
+                    <h1>Hiruna</h1>
+                    <p>24 min watched</p>
+                </div>
+            </div>
             <div className={classes.EmailContainer}>email</div>
             <div className={classes.GameAttributesContainer}>game attributes</div>
             <button>Log out</button>
