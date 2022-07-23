d4e4a01c58f84ae618cc8f5471a035a40a3d1b33
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-23 18:29:50 +1200

    Updated console logs + added timeout + retry

--- src/chrome/panoptoPage.js
+++ src/chrome/panoptoPage.js
@@ -1,4 +1,4 @@
-console.log("Loading CONTENT SCRIPT panoptoPage");
+console.log("[panoptoPage.js] Loading content script");
 
 // the payout time period in seconds
 const PAYOUT_TIME_PERIOD = 10000;
@@ -8,7 +8,7 @@ let lastIntervalTime = 0;
 let remainingTime = 10000;
 
 function handlePlay() {
-  console.log("Handle play called");
+  console.log("[panoptoPage.js] timeout for pay has started");
   if (payTimeoutId !== 0) {
     payTimeoutId = setTimeout(triggerPandaPayout, PAYOUT_TIME_PERIOD);
   } else {
@@ -20,32 +20,40 @@ function handlePause() {
   let pauseTime = Date.now();
   clearTimeout(payTimeoutId);
   let timePassed = Math.floor(pauseTime - lastIntervalTime);
-  console.log("timePassed: ", timePassed);
   remainingTime = PAYOUT_TIME_PERIOD - timePassed;
-  console.log("remainingTime: ", remainingTime);
+  console.log("[panoptoPage.js] remainingTime before next payout: ", remainingTime);
   payTimeoutId = 0;
 }
 
 function triggerPandaPayout() {
   lastIntervalTime = Date.now();
-  console.log("CONGRATS HERE IS 10 PANDA BUCKS");
+  console.log("[panoptoPage.js] Trigger pandabucks payout");
   // Trigger a pop up box that the user needs to click to send a post to the backedn
   // This will increment the users pandabucks by 50, every 5 minutes
   payTimeoutId = setTimeout(triggerPandaPayout, PAYOUT_TIME_PERIOD);
 }
 
 window.setTimeout(() => {
-  console.log("time out over");
-  const video = document.getElementById("secondaryVideo");
-  console.log(video);
-  if (video) {
-    console.log("adding event listener");
-    video.addEventListener("play", (event) => {
-      handlePlay();
-    });
-    video.addEventListener("pause", (event) => {
-      handlePause();
-    });
+  addEventListeners();
+}, 4000);
+
+function addEventListeners(attempt = 0) {
+  if (attempt < 3) {
+    console.log("[panoptoPage.js] Timeout finished, check for video element");
+    const video = document.getElementById("secondaryVideo");
+    console.log(video);
+    if (video) {
+      video.addEventListener("play", (event) => {
+        handlePlay();
+      });
+      video.addEventListener("pause", (event) => {
+        handlePause();
+      });
+    } else {
+      console.log("[panoptoPage.js] Timeout finished, element not found trying again in 3 seconds");
+      addEventListeners(attempt + 1);
+    }
+  } else {
+    console.log("[panoptoPage.js] Unable to find video element in 3 attempts");
   }
-  console.log("CONTENT SCRIPT panoptoPage run");
-}, 7000);
+}
