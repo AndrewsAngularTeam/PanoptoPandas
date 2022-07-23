0629a182fc36a6d9b2e16c2b41838074452d3c94
ff-dev-45 <flynn.fromont@gmail.com>
2022-07-23 18:15:36 +1200

    got working on panopto content script

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,40 +1,4 @@
-import { clearTimeout } from "timers";
-
 export {};
-
-// the payout time period in seconds
-const PAYOUT_TIME_PERIOD = 10000;
-
-let payTimeoutId = -1;
-let lastIntervalTime = 0;
-let remainingTime = 0;
-
-function handlePlay() {
-  console.log("Handle play called");
-  if (payTimeoutId !== 0) {
-    payTimeoutId = window.setTimeout(triggerPandaPayout, PAYOUT_TIME_PERIOD);
-  } else {
-    payTimeoutId = window.setTimeout(triggerPandaPayout, remainingTime);
-  }
-}
-
-function handlePause() {
-  let pauseTime = Date.now();
-  clearTimeout(payTimeoutId);
-  let timePassed = Math.floor((pauseTime - lastIntervalTime) / 1000);
-  console.log("timePassed: ", timePassed);
-  remainingTime = PAYOUT_TIME_PERIOD - timePassed;
-  console.log("remainingTime: ", remainingTime);
-  payTimeoutId = 0;
-}
-
-function triggerPandaPayout() {
-  lastIntervalTime = Date.now();
-  console.log("CONGRATS HERE IS 10 PANDA BUCKS");
-  // Trigger a pop up box that the user needs to click to send a post to the backedn
-  // This will increment the users pandabucks by 50, every 5 minutes
-  payTimeoutId = window.setTimeout(triggerPandaPayout, PAYOUT_TIME_PERIOD);
-}
 
 /** Fired when the extension is first installed,
  *  when the extension is updated to a new version,
@@ -49,23 +13,6 @@ chrome.runtime.onConnect.addListener((port) => {
 
 chrome.runtime.onStartup.addListener(() => {
   console.log("[background.js] onStartup");
-});
-
-// This is for background receiving a message from a content script?
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
-  if (message.type === "PLAY") {
-    console.log("VIDEO PLAYING");
-    handlePlay();
-    sendResponse("VIDEO PLAY RECEIVED");
-  } else if (message.type === "PAUSE") {
-    console.log("VIDEO PAUSED");
-    handlePause();
-    sendResponse("VIDEO PAUSE RECEIVED");
-  } else {
-    console.log("NOT RECOGNISED");
-    sendResponse("EVEN NOT RECOGNISED");
-  }
 });
 
 /**
--- src/chrome/panoptoPage.js
+++ src/chrome/panoptoPage.js
@@ -1,5 +1,39 @@
 console.log("Loading CONTENT SCRIPT panoptoPage");
 
+// the payout time period in seconds
+const PAYOUT_TIME_PERIOD = 10000;
+
+let payTimeoutId = -1;
+let lastIntervalTime = 0;
+let remainingTime = 10000;
+
+function handlePlay() {
+  console.log("Handle play called");
+  if (payTimeoutId !== 0) {
+    payTimeoutId = setTimeout(triggerPandaPayout, PAYOUT_TIME_PERIOD);
+  } else {
+    payTimeoutId = setTimeout(triggerPandaPayout, remainingTime);
+  }
+}
+
+function handlePause() {
+  let pauseTime = Date.now();
+  clearTimeout(payTimeoutId);
+  let timePassed = Math.floor(pauseTime - lastIntervalTime);
+  console.log("timePassed: ", timePassed);
+  remainingTime = PAYOUT_TIME_PERIOD - timePassed;
+  console.log("remainingTime: ", remainingTime);
+  payTimeoutId = 0;
+}
+
+function triggerPandaPayout() {
+  lastIntervalTime = Date.now();
+  console.log("CONGRATS HERE IS 10 PANDA BUCKS");
+  // Trigger a pop up box that the user needs to click to send a post to the backedn
+  // This will increment the users pandabucks by 50, every 5 minutes
+  payTimeoutId = setTimeout(triggerPandaPayout, PAYOUT_TIME_PERIOD);
+}
+
 window.setTimeout(() => {
   console.log("time out over");
   const video = document.getElementById("secondaryVideo");
@@ -7,14 +41,10 @@ window.setTimeout(() => {
   if (video) {
     console.log("adding event listener");
     video.addEventListener("play", (event) => {
-      chrome.runtime.sendMessage({ type: "PLAY" }, function (response) {
-        console.log("RESPONSE: ", response);
-      });
+      handlePlay();
     });
     video.addEventListener("pause", (event) => {
-      chrome.runtime.sendMessage({ type: "PAUSE" }, function (response) {
-        console.log("RESPONSE: ", response);
-      });
+      handlePause();
     });
   }
   console.log("CONTENT SCRIPT panoptoPage run");
