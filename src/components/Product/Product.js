5ff4c4ac66e785a25d3f104325be10641a4f77ab
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-23 21:28:16 +1200

    add lock assets

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,8 @@ import React from 'react';
 import classes from './Product.module.scss';
 import Gem from "../../assets/gem.svg"
 import PlaceholderProduct from "../../assets/placeholderProduct.png"
+import Lock from "../../assets/lock.svg"
+import Unlock from "../../assets/unlock.svg"
 
 const Product = (props) => {
     return (
@@ -12,6 +14,7 @@ const Product = (props) => {
                     <img src={Gem} />
                     <p>600</p>
                 </div>
+                <img src={Lock} />
             </div>
         </div>
     )
