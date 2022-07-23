a77db55cba3be436c7252602523a92487317e4f4
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-07-23 18:00:55 +1200

    init navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   const handlePopup = () => {
@@ -17,15 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <button onClick={handlePopup}>POPUP</button>
-      </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from "./NavBar.module.scss"
+
+import Logo from "../../assets/logo.svg"
+import DefaultPic from "../../assets/placeholderAvatar.png";
+
+const NavBar = () => {
+    return (
+        <header className={classes.NavBar}>
+            <div className={classes.LogoContainer}>
+                <img src={Logo} />
+                <p>Pandas</p>
+            </div>
+            <nav className={classes.NavContainer}>
+                <ul>
+                    <li>Customisation</li>
+                    <li>Leaderboard</li>
+                </ul>
+            </nav>
+
+            <div className={classes.ProfileContainer}>
+                <img src={DefaultPic} />
+                <p>Name Here</p>
+            </div>
+
+        </header >
+    )
+}
+
+export default NavBar;
